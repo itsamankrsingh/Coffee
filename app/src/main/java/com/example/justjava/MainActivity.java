63ee41905c37457afd112f1acc0cd78ca3541962
Aman Kumar Singh <itsamanrsingh@gmail.com>
2020-01-01 11:28:19 +0530

    Third Commit

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.justjava;
 
 
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,12 +16,16 @@ import androidx.appcompat.app.AppCompatActivity;
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
-    int quantity=0;
+    int quantity=1;
+    boolean cream_checked,choco_checked;
+    String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
     /**
@@ -25,23 +33,71 @@ public class MainActivity extends AppCompatActivity {
      */
     public void submitOrder(View view) {
         String summary=createOrderSummary(quantity);
-        displayMessage(summary);
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java Order Summary for: "+name);
+        intent.putExtra(Intent.EXTRA_TEXT,summary);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
-    public void increment(View view) {
-        quantity=quantity+1;
-        displayQuantity(quantity);
+
+    /*Whipped Cream Checkox*/
+    public void checkBox(View v){
+        CheckBox whipped_cream=findViewById(R.id.whipped);
+        cream_checked=whipped_cream.isChecked();
     }
+
+    /*Chocolate Checkbox*/
+    public void chocoCheck(View v){
+        CheckBox choco=findViewById(R.id.choclate);
+        choco_checked=choco.isChecked();
+    }
+
+
+        public void increment(View view) {
+            if (quantity <100) {            //Max number of coffee is 100
+                quantity = quantity + 1;
+                displayQuantity(quantity);
+            }
+            if (quantity == 100) {
+                // Show an error message as a toast
+                Toast.makeText(this, "You cannot have more than 100 coffee", Toast.LENGTH_SHORT).show();
+                // Exit this method early because there's nothing left to do
+                return;
+            }
+        }
 
     public void decrement(View view) {
-        quantity=quantity-1;
-        displayQuantity(quantity);
+        if(quantity>1){    //Min number of coffee is 1
+            quantity = quantity - 1;
+            displayQuantity(quantity);
+        }
+        if (quantity == 1) {
+            // Show an error message as a toast
+            Toast.makeText(this, "You cannot have less than 1 coffee", Toast.LENGTH_SHORT).show();
+            // Exit this method early because there's nothing left to do
+            return;
+        }
     }
     /**
      calculate price
      */
     private int calculatePrice(){
-        int price=quantity*10;
+        int price;
+        if(cream_checked&&choco_checked){
+            price=quantity*30;
+        }
+        else if(cream_checked){
+            price=quantity*25;
+        }
+        else if(choco_checked){
+            price=quantity*25;
+        }
+        else{
+            price=quantity*20;
+        }
         return price;
     }
 
@@ -53,21 +109,31 @@ public class MainActivity extends AppCompatActivity {
         quantityTextView.setText("" + number);
     }
 
-    /**
-     * This method displays the given text on the screen.
-     */
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
-    }
+
     /**Order summary*/
     private String createOrderSummary(int numberOfCoffee){
+        boolean creamHasChecked,chocoHasChecked;
         int price=calculatePrice();
-        String name="Name: Aman Singh";
         int number=numberOfCoffee;
-        String priceMessage=name+"\nQunatity= "+number+"\nTotal: ₹"+price+"\nThank You!";
+        if(cream_checked){
+            creamHasChecked=true;
+        }
+        else{
+           creamHasChecked=false;
+        }
+        if(choco_checked){
+            chocoHasChecked=true;
+        }
+        else{
+            chocoHasChecked=false;
+        }
+        EditText user=findViewById(R.id.username_view);
+        name=user.getText().toString();
+        String priceMessage="Name: "+name+"\nAdd Whipped Cream?"+creamHasChecked+"\nAdd Chocolate?"+chocoHasChecked+"\nQunatity= "+number+"\nTotal: ₹"+price+"\nThank You!";
         return priceMessage;
 
     }
+
+
 
 }
